2a119fc9fd95f9cc153479558171ecd23eb924ea
unknown <daltonagray@gmail.com>
2023-07-05 22:05:13 +0100

    refactored openai_api.ipynb

--- edit.py
+++ edit.py
@@ -9,8 +9,12 @@ np.random.seed(42)
 # Create a DataFrame with random values
 df = pd.DataFrame(np.random.randn(1000000, 5), columns=['A', 'B', 'C', 'D', 'E'])
 
-# Add 10 to each value in the DataFrame using vectorization
-df += 10
+# # Iterate through the DataFrame and add 10 to each row of each column
+# t = time.time()
+# for index, row in df.iterrows():
+#     df.loc[index] = row + 10
+# end = time.time()
+# print(end - t)
 
 print(df)
 end = time.time()
